f9f263f9f07a899946dface2c3ecf262fac6497d
unknown <parilovespakhi@gmail.com>
2020-09-25 14:02:01 +0530

    FizzBuzz

---
+++ Month.java
@@ -0,0 +1,8 @@
+class Month{
+	public static void main(String args[]){
+		String month[]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+		for(int i=0; i<month.length; i++){
+			System.out.println(month[i]);
+		}
+	}
+}
