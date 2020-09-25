5f3062d57d15ca5dcb367b5a1963131413ab271f
unknown <parilovespakhi@gmail.com>
2020-09-25 14:07:02 +0530

    FizzBuzz2

---
+++ FizzBuzzTwo.java
@@ -0,0 +1,15 @@
+class FizzBuzz {
+  public static void main(String args[]) {
+    for(int a=1; i<51; i++){
+     if (a%15==0) {
+      System.out.println("FizzBuzz");
+     }
+     else if (a%5==0) {
+      System.out.println("Buzz");
+     }
+     else if (a%3==0) {
+      System.out.println("Fizz");
+     }
+    }
+  }
+}
